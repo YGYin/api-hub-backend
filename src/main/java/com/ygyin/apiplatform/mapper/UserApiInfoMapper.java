eba69ff46c805d18b40c4eb44413d90d6d770cf9
YGYin <ygyin0307@gmail.com>
2024-07-11 15:57:36 +0800

    User API info mapper updated

---
+++ src/main/java/com/ygyin/apiplatform/mapper/UserApiInfoMapper.java
@@ -0,0 +1,18 @@
+package com.ygyin.apiplatform.mapper;
+
+import com.ygyin.apiplatform.model.entity.UserApiInfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author yg
+* @description 针对表【user_api_info(用户调用接口信息)】的数据库操作Mapper
+* @createDate 2024-07-08 23:32:59
+* @Entity com.ygyin.apiplatform.model.entity.UserApiInfo
+*/
+public interface UserApiInfoMapper extends BaseMapper<UserApiInfo> {
+
+}
+
+
+
+
