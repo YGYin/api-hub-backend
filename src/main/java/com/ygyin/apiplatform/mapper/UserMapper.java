24ba296cad3c4eaf612181622743993e7ea6d876
YGYin <ygyin0307@gmail.com>
2024-07-11 15:57:46 +0800

    User mapper updated

---
+++ src/main/java/com/ygyin/apiplatform/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.ygyin.apiplatform.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ygyin.apiplatform.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
