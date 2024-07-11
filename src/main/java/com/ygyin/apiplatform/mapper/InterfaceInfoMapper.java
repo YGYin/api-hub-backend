ba0255031337eb2cd80347e7a8246fef71d6f0cc
YGYin <ygyin0307@gmail.com>
2024-07-11 15:57:19 +0800

    Interface Info mapper updated

---
+++ src/main/java/com/ygyin/apiplatform/mapper/InterfaceInfoMapper.java
@@ -0,0 +1,18 @@
+package com.ygyin.apiplatform.mapper;
+
+import com.ygyin.apiplatform.model.entity.InterfaceInfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author yg
+* @description 针对表【interface_info(接口信息)】的数据库操作Mapper
+* @createDate 2024-06-19 12:13:24
+* @Entity com.ygyin.apiplatform.model.entity.InterfaceInfo
+*/
+public interface InterfaceInfoMapper extends BaseMapper<InterfaceInfo> {
+
+}
+
+
+
+
