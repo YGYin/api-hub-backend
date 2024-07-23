ad5861b27a2f758f2d86f2e45cc0c91bbb98cf19
YGYin <ygyin0307@gmail.com>
2024-07-23 17:30:16 +0800

    Update the mapper for interface query

--- src/main/java/com/ygyin/apiplatform/mapper/UserApiInfoMapper.java
+++ src/main/java/com/ygyin/apiplatform/mapper/UserApiInfoMapper.java
@@ -3,6 +3,8 @@ package com.ygyin.apiplatform.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.ygyin.apiplatformcommon.model.entity.UserApiInfo;
 
+import java.util.List;
+
 /**
 * @author yg
 * @description 针对表【user_api_info(用户调用接口信息)】的数据库操作Mapper
@@ -11,6 +13,9 @@ import com.ygyin.apiplatformcommon.model.entity.UserApiInfo;
 */
 public interface UserApiInfoMapper extends BaseMapper<UserApiInfo> {
 
+    // select apiId, sum(totalNum) as totalCallNum
+    // from user_api_info group by apiId order by totalCallNum desc limit 3
+    List<UserApiInfo> listApiDescByCallNum(int topLimit);
 }
 
 
